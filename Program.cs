6346af0a8f7ac35180e5773f6a72c50f1f31cb73
Chris Romp <ChrisRomp@users.noreply.github.com>
2022-08-10 11:30:59 -0700

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Net;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+class TestClass
+{
+    public static void Test()
+    {
+        var data = JObject.Parse(GetJson());
+        var values = (JArray)data["data"]["values"];
+        var valCount = values.Count();
+        
+        var json = "{ \"totalCount\": " + valCount + " }";
+        Console.Write(json);
+    }
+
+    static string GetJson()
+    {
+        var json = @"{
+    ""status"": 200,
+    ""type"": ""Success"",
+    ""message"": ""OK"",
+    ""data"": {
+        ""values"": [
+            {
+                ""Code"": 14111,
+                ""Quantity"": 5
+            },
+            {
+                ""Code"": 14110,
+                ""Quantity"": 5
+            },
+            {
+                ""Code"": 14107,
+                ""Quantity"": 50
+            }
+        ],
+        ""pageInfo"": {
+            ""totalSize"": 15,
+            ""pageSize"": 10,
+            ""pageNum"": 1
+        }
+    }
+}"; // PASTE JSON HERE - USE "" FOR QUOTES
+        return json;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Environment version: " + Environment.Version);
+        Console.WriteLine("Json.NET version: " + typeof(JsonSerializer).Assembly.FullName);
+        Console.WriteLine("");
+
+        TestClass.Test();
+    }
+
+}
